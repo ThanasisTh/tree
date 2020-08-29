5e2bb9006aa09b96025fd0f65b5d3efa44cd78b6
thanasis - s193510 <thanasisdtl@gmail.com>
2020-08-29 17:54:38 +0200

    Added barebones plug tests

--- lib/endpoint.ex
+++ lib/endpoint.ex
@@ -14,6 +14,37 @@ defmodule TREE.Endpoint do
     send_resp(conn, 200, "pong!")
   end
 
+  # Handle incoming events, if the payload is the right shape, process the
+  # events, otherwise return an error.
+  post "/events" do
+    {status, body} =
+      case conn.body_params do
+        %{"events" => events} -> {200, process_events(events)}
+        _ -> {422, missing_events()}
+      end
+
+    send_resp(conn, status, body)
+  end
+
+  defp process_events(events) when is_list(events) do
+    # Do some processing on a list of events
+    Poison.encode!(%{response: "Received Events!"})
+  end
+
+  defp process_events(_) do
+    # If we can't process anything, let them know :)
+    Poison.encode!(%{response: "Please Send Some Events!"})
+  end
+
+  defp missing_events do
+    Poison.encode!(%{error: "Expected Payload: { 'events': [...] }"})
+  end
+
+
+
+
+
+
   match _ do
     send_resp(conn, 404, "oops")
   end
