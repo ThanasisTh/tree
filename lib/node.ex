7b110c933f6716ebc6a7cee657c07ee1a7ce5764
thanasis - s193510 <thanasisdtl@gmail.com>
2020-08-29 14:20:24 +0200

    Added comments

--- lib/node.ex
+++ lib/node.ex
@@ -1,9 +1,10 @@
 defmodule TREE.NODE do
-
   defstruct [:left, :value, :right]
 
+  @typedoc "Represents node struct."
   @type t :: %__MODULE__{
-    left: t() | nil,
-    value: TREE.element(),
-    right: t() | nil}
+          left: t() | nil,
+          value: TREE.element(),
+          right: t() | nil
+        }
 end
--- lib/tree.ex
+++ lib/tree.ex
@@ -1,43 +1,80 @@
 defmodule TREE do
   @moduledoc """
-  Documentation for `TREE`.
+  Basic binary tree with insert operation.
   """
 
   alias TREE.NODE
 
-  defstruct [:current, :root]
+  defstruct [:compare, :root]
 
+  @typedoc "Data stored in a node."
   @type element :: term()
-  @type current :: (a :: element(), b :: element() -> integer())
-  @type tree :: %__MODULE__{root: NODE.t() | nil, current: current()}
 
-  @spec new([element()], current()) :: tree()
-  def new(elements \\ [], current \\ fn a, b -> a - b end)
-      when is_list(elements) and is_function(current) do
-    tree = %__MODULE__{current: current}
+  @typedoc "Checks whether current node should be stored in right or left subtree (comparing function)."
+  @type compare :: (a :: element(), b :: element() -> integer())
+
+  @typedoc "Tree."
+  @type tree :: %__MODULE__{root: NODE.t() | nil, compare: compare()}
+
+  @doc """
+  Creates new tree
+
+  ## Parameters
+
+    - elements: List of data to be stored in tree.
+    - compare: Comparing function
+
+  ## Examples
+
+      iex> tree = BST.new([0])
+      iex> tree.root
+      %BST.Node{left: nil, value: 0, right: nil}
+
+      iex> tree = BST.new([0, 1])
+      iex> tree.root
+      %BST.Node{data: 0, left: nil, right: %BST.Node{data: 1, left: nil, right: nil}}
+  """
+  @spec new([element()], compare()) :: tree()
+  def new(elements \\ [], compare \\ fn a, b -> a - b end)
+      when is_list(elements) and is_function(compare) do
+    tree = %__MODULE__{compare: compare}
 
     Enum.reduce(elements, tree, fn element, tree ->
       insert(tree, element)
     end)
   end
 
+
+  @doc """
+  Adds a node to a tree.
+
+  Conflicts with old nodes resolved by comparing and replacing.
+  Defaults to replacing with new element.
+
+  ## Examples
+      iex> tree = BST.new([1])
+      iex> tree = BST.insert(tree, 2)
+      iex> tree.root
+      %BST.Node{data: 1, left: nil, right: %BST.Node{data: 2, left: nil, right: nil}}
+  """
   @spec insert(tree(), element(), (element(), element() -> element())) :: tree()
   def insert(%__MODULE__{} = tree, element, fun \\ fn _a, b -> b end) do
-    %__MODULE__{tree | root: insert_node(tree.root, element, tree.current, fun)}
+    %__MODULE__{tree | root: insert_node(tree.root, element, tree.compare, fun)}
   end
 
-  defp insert_node(nil, element, _current, _fun), do: %NODE{value: element}
+  defp insert_node(nil, element, _compare, _fun), do: %NODE{value: element}
 
-  defp insert_node(%NODE{left: left, value: elem1, right: right} = node, elem2, current, fun) do
-    case compare(elem2, elem1, current) do
+  defp insert_node(%NODE{left: left, value: elem1, right: right} = node, elem2, compare, fun) do
+    case compare(elem2, elem1, compare) do
       :eq -> %NODE{node | value: fun.(elem1, elem2)}
-      :lt -> %NODE{node | left: insert_node(left, elem2, current, fun)}
-      :gt -> %NODE{node | right: insert_node(right, elem2, current, fun)}
+      :lt -> %NODE{node | left: insert_node(left, elem2, compare, fun)}
+      :gt -> %NODE{node | right: insert_node(right, elem2, compare, fun)}
     end
   end
 
-  defp compare(a, b, current) do
-    val = current.(a, b)
+
+  defp compare(a, b, compare) do
+    val = compare.(a, b)
 
     cond do
       val == 0 -> :eq
@@ -47,7 +84,3 @@ defmodule TREE do
   end
 
 end
-
-tree = TREE.new([0])
-tree = TREE.insert(tree, 2)
-tree.root
